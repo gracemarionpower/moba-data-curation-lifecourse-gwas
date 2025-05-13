a170ef1f1d6d73004aa5d799c05198907fece613
gracemarionpower <88652994+gracemarionpower@users.noreply.github.com>
2025-05-13 11:05:55 +0200

    Create generate_static_covariate.R

---
+++ generate_static_covariate.R
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------------
+# Script Name: generate_static_covariate.R
+# Purpose    : To generate a static_covariate.txt file for the Lifecourse GWAS pipeline
+# Date       : 2025-05-13
+# Author     : Grace M. Power
+# Collaborators: Marc Vaudel and Stefan Johansson, University of Bergen
+# --------------------------------------------------------------------------------------
