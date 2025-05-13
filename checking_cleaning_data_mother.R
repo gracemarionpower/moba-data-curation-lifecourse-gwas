4f8e744d45d83e5fbbbfef8bb797d522ed06fe07
gracemarionpower <88652994+gracemarionpower@users.noreply.github.com>
2025-05-13 16:32:55 +0200

    Update checking_cleaning_data_mother.R

--- checking_cleaning_data_mother.R
+++ checking_cleaning_data_mother.R
@@ -1,7 +1,7 @@
 
 # --------------------------------------------------------------------------------------
 # Script Name : checking_cleaning_data_mother.R
-# Purpose     : To identify the best data sources and clean parental data before curating time-varying datasets
+# Purpose     : To identify the best data sources and clean maternal data before curating time-varying datasets
 # Date created: 13-05-2025
 # Last updated: 13-05-2025
 # Author      : Grace M. Power
