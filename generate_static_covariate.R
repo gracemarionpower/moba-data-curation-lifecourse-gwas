1cae89bb4913e87fb00718ac6b639028271e4c5c
gracemarionpower <88652994+gracemarionpower@users.noreply.github.com>
2025-05-13 11:46:47 +0200

    Update generate_static_covariate.R

--- generate_static_covariate.R
+++ generate_static_covariate.R
@@ -1,7 +1,69 @@
+# In bash, run: R
+
 # --------------------------------------------------------------------------------------
 # Script Name: generate_static_covariate.R
-# Purpose    : To generate a static_covariate.txt file for the Lifecourse GWAS pipeline
-# Date       : 2025-05-13
-# Author     : Grace M. Power
+# Purpose      : To generate a static_covariate.txt file for the Lifecourse GWAS pipeline
+# Date created : 13-05-2025
+# Last updated : 13-05-2025
+# Author       : Grace M. Power
 # Collaborators: Marc Vaudel and Stefan Johansson, University of Bergen
 # --------------------------------------------------------------------------------------
+
+# Define input file paths
+input_file <- "/home/grace.power/archive/moba/pheno/v12/pheno_anthropometrics_25-05-07_Mikko/mfr.gz"
+psam_file  <- "/home/grace.power/archive/moba/geno/HDGB-MoBaGenetics/2024.12.03/moba_genotypes_2024.12.03_common.psam"
+output_file <- "/home/grace.power/work/gpower/data/lifecourse_gwas_data_curation/static_covariate.txt"
+
+# Read compressed mfr file
+con <- gzfile(input_file, "rt")
+mfr <- read.delim(con, stringsAsFactors = FALSE)
+close(con)
+
+# Read the .psam file to get FID-IID mapping
+psam <- read.table(psam_file, header = TRUE, stringsAsFactors = FALSE, comment.char = "")
+colnames(psam)[1] <- "FID"  # Rename '#FID' to 'FID'
+
+# Create list of rows from mfr
+rows <- list()
+
+for (i in 1:nrow(mfr)) {
+  child_id  <- mfr$child_sentrix_id[i]
+  mother_id <- mfr$mother_sentrix_id[i]
+  father_id <- mfr$father_sentrix_id[i]
+  child_sex <- mfr$sex[i]
+  child_yob <- mfr$birth_year[i]
+  mother_yob <- mfr$mother_birth_year[i]
+  father_yob <- mfr$father_birth_year[i]
+
+  # Add child
+  rows[[length(rows) + 1]] <- c(IID = child_id, sex = child_sex, yob = child_yob)
+  # Add mother
+  if (!is.na(mother_id)) {
+    rows[[length(rows) + 1]] <- c(IID = mother_id, sex = 2, yob = mother_yob)
+  }
+  # Add father
+  if (!is.na(father_id)) {
+    rows[[length(rows) + 1]] <- c(IID = father_id, sex = 1, yob = father_yob)
+  }
+}
+
+# Convert to data frame
+static_covariate <- as.data.frame(do.call(rbind, rows), stringsAsFactors = FALSE)
+
+# Convert types
+static_covariate$sex <- as.integer(static_covariate$sex)
+static_covariate$yob <- as.integer(static_covariate$yob)
+
+# Merge FID using psam file
+static_covariate <- merge(psam[, c("FID", "IID")], static_covariate, by = "IID")
+
+# Order by FID and write output
+static_covariate <- static_covariate[order(static_covariate$FID), ]
+
+# Reorder columns
+static_covariate <- static_covariate[, c("FID", "IID", "sex", "yob")]
+
+write.table(static_covariate, file = output_file, sep = "\t", row.names = FALSE, quote = FALSE)
+
+
+
